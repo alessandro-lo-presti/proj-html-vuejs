5338528bed94a322101f68d69151374de034783a
alessandro-lo-presti <mitlopresti@hotmail.it>
2021-02-16 15:39:49 +0100

    why-section vue

--- js/script.js
+++ js/script.js
@@ -57,6 +57,32 @@ var app = new Vue({
         special: false
       }
     ],
+    whyCardData: [
+      {
+        icon: "fas fa-briefcase",
+        title: "Entrepreneurship",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        
+      },
+      {
+        icon: "fas fa-rocket",
+        title: "Accelerated Learning",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+      },
+      {
+        icon: "fas fa-thumbs-up",
+        title: "Productivity",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+      },
+      {
+        icon: "fas fa-cog",
+        title: "Life Coaching",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+
+      }
+    ],
     feedbackData: [
       {
         icon: "fas fa-user-friends",
